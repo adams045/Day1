9357d544629e2ee7ae9c704e316286f32b5f001a
adams045 <shanadams64683@gmail.com>
2024-12-26 20:43:53 +0530

    added

--- Bitwise.java
+++ Bitwise.java
@@ -4,7 +4,9 @@ public class Bitwise{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number=sc.nextInt();
+        int number2=sc.nextInt();
         System.out.println(((number>>2)));
         System.out.println((number<<2));
+        System.out.println(number ^ number2);
     }
 }
