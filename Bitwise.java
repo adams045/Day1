a75f03590aedb70ebc76cc93030c29f798d27048
adams045 <shanadams64683@gmail.com>
2024-12-26 20:37:57 +0530

    added

--- Bitwise.java
+++ Bitwise.java
@@ -1,1 +1,10 @@
-
+package Day1;
+import java.util.*;
+public class Bitwise{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number=sc.nextInt();
+        System.out.println(((number>>2)));
+        System.out.println((number<<2));
+    }
+}
